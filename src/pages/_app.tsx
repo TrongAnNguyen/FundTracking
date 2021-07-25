235c270f8313dfd1668ce678ccc78e53e585f85a
An Nguyen <nguyentrongan010@gmail.com>
2021-07-25 16:25:49 +0700

    feat: setup redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import ErrorBoundary from 'components/ErrorBoundary'
+import { StrictMode } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary>
-      <Component {...pageProps} />
-    </ErrorBoundary>
+    <StrictMode>
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </StrictMode>
   )
 }
 export default MyApp
---
+++ src/state/user/hooks.tsx
@@ -0,0 +1,3 @@
+export const useIsDarkMode = (): Boolean => {
+  return false
+}
